353e713f3a531a07b2824143cd7caf0e21681ea5
jeancigoli <jean@primi.com.br>
2021-02-19 21:08:03 -0300

    refactor: move succes http response to a helper method

--- src/presentation/controllers/signUp/signup.ts
+++ src/presentation/controllers/signUp/signup.ts
@@ -2,7 +2,7 @@ import {
   HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount,
 } from './signup-protocols';
 import { MissingParamError, InvalidParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http-helper';
+import { badRequest, created, serverError } from '../../helpers/http-helper';
 
 export class SignUpController implements Controller {
   constructor(
@@ -45,10 +45,7 @@ export class SignUpController implements Controller {
         password,
       });
 
-      return {
-        statusCode: 201,
-        body: account,
-      };
+      return created(account);
     } catch (error) {
       return serverError();
     }
